a696d7981cd3f51bf36f2b66af581821c805e73c
shubhamsharnagate <shubhamsharnagate9021@gamil.com>
2026-06-25 23:53:23 +0530

    atales password change

--- hostel_complaint_system/config.py
+++ hostel_complaint_system/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'hostel_secret_key_123_abc')
-    MONGO_URI = "mongodb+srv://hostel123:hostel123@cluster0.somsoqv.mongodb.net/?appName=Cluster0"
+    MONGO_URI = "mongodb+srv://hostel123:shubham123@cluster0.somsoqv.mongodb.net/?appName=Cluster0"
     DATABASE_NAME = 'hostel_complaint_db'
