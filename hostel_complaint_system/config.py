4e326f91891f1ac365977779b59459f7fe9f3788
shubhamsharnagate <shubhamsharnagate9021@gamil.com>
2026-06-25 23:17:19 +0530

    Added MongoDB Atlas

--- hostel_complaint_system/config.py
+++ hostel_complaint_system/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'hostel_secret_key_123_abc')
-    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/')
+    MONGO_URI = "mongodb+srv://hostel123:hostel123@cluster0.somsoqv.mongodb.net/?appName=Cluster0"
     DATABASE_NAME = 'hostel_complaint_db'
